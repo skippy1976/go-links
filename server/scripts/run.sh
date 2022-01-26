3b1fccc953fcaa8f2be5588beaa17580d0659123
skippy1976 <github@harteveldt.com>
2022-01-26 15:02:05 -0500

    Fixed script issue with cloud run
    Change cd to cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../src"

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -3,7 +3,7 @@
 source server/scripts/load_secrets.sh
 
 export FLASK_APP=main.py
-cd server/src
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../src"
 
 sh ../scripts/upgrade_db.sh
 
--- server/scripts/upgrade_db.sh
+++ server/scripts/upgrade_db.sh
@@ -1,4 +1,4 @@
-cd server/src
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../src"
 
 # core db
 export FLASK_APP=main.py
