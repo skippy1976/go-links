52f875241e76e27a5fe45f8ed32ea5b32366f3eb
skippy1976 <github@harteveldt.com>
2022-01-27 09:02:20 -0500

    trying to fix issue with script

--- server/scripts/upgrade_db.sh
+++ server/scripts/upgrade_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+pwd
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../src"
-
+pwd
 # core db
 export FLASK_APP=main.py
-
+echo "starting flask"
 flask db upgrade
